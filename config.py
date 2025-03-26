0389e6e66e118b3015255477b526ffa71259f840
EN <tnrkip@yandex.ru>
2025-03-26 11:19:27 +0300

    добавлен Кравец и Плетминцев

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 API_TOKEN = '7668308066:AAFCqYsqUnnvWoi_ivFs8YSeW8CebJcOWsI'
 BASE_DIR = 'files'
 ADMINS = [
-    5233776312
+    5233776312,
+    1099173284, # Плетминцев
+    5233776312, # Кравец
 ]
 
